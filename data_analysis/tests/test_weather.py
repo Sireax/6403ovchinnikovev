da1e9e968c3bb812b6194927da4778c47b5dbc68
Sireax <sireax@yandex.ru>
2024-12-03 12:56:37 +0400

    tests

--- data_analysis/tests/test_weather.py
+++ data_analysis/tests/test_weather.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from data_analysis.module.weather_code import DataAnalyzer
+from data_analysis.module.data_analyzer import DataAnalyzer
 
 
 class TestWeatherData(unittest.TestCase):
@@ -10,23 +10,23 @@ class TestWeatherData(unittest.TestCase):
             'tavg': [20, 22, 21, 23, 24, 22, 20, 19, 21, 23]
         }
         self.df = pd.DataFrame(data)
-        self.weather_data = DataAnalyzer(self.df)
+        self.data_analyzer = DataAnalyzer(self.df)
 
     def test_get_data(self):
-        result = self.weather_data.get_data()
+        result = self.data_analyzer.get_data()
         self.assertTrue(result.equals(self.df))
 
     def test_moving_average(self):
-        self.weather_data.moving_average(3)
-        self.assertIn('moving_average_3', self.weather_data.data.columns)
+        self.data_analyzer.moving_average(3)
+        self.assertIn('moving_average_3', self.data_analyzer.data.columns)
 
     def test_calculate_difference(self):
-        self.weather_data.calculate_difference()
-        self.assertIn('temperature_difference', self.weather_data.data.columns)
+        self.data_analyzer.calculate_difference()
+        self.assertIn('temperature_difference', self.data_analyzer.data.columns)
 
     def test_autocorrelation(self):
-        self.weather_data.auto_corr(1, 1)
-        self.assertIn('autocorrelation_lag_1', self.weather_data.data.columns)
+        self.data_analyzer.auto_corr(1, 1)
+        self.assertIn('autocorrelation_lag_1', self.data_analyzer.data.columns)
 
 
 if __name__ == '__main__':
